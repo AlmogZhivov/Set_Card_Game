22ca4d72a730279ed93d1c3d74fa3d2d22d39341
Aviad Lampert <aviadlam@post.bgu.ac.il>
2024-02-14 14:43:42 +0200

    commiting thread safe table 1

--- src/main/java/bguspl/set/ex/Table.java
+++ src/main/java/bguspl/set/ex/Table.java
@@ -3,6 +3,7 @@ package bguspl.set.ex;
 import bguspl.set.Env;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -32,18 +33,29 @@ public class Table {
      */
     protected final Integer[] cardToSlot; // slot per card (if any)
 
+    private final HashMap<Pair, Boolean> tokens;
+
+    private final Object cardsLock;
+
+    private final Object hashLock;
+
     /**
      * Constructor for testing.
      *
      * @param env        - the game environment objects.
-     * @param slotToCard - mapping between a slot and the card placed in it (null if none).
-     * @param cardToSlot - mapping between a card and the slot it is in (null if none).
+     * @param slotToCard - mapping between a slot and the card placed in it (null if
+     *                   none).
+     * @param cardToSlot - mapping between a card and the slot it is in (null if
+     *                   none).
      */
     public Table(Env env, Integer[] slotToCard, Integer[] cardToSlot) {
 
         this.env = env;
         this.slotToCard = slotToCard;
         this.cardToSlot = cardToSlot;
+        this.tokens = new HashMap<Pair, Boolean>();
+        this.cardsLock = new Object();
+        this.hashLock = new Object();
     }
 
     /**
@@ -57,15 +69,18 @@ public class Table {
     }
 
     /**
-     * This method prints all possible legal sets of cards that are currently on the table.
+     * This method prints all possible legal sets of cards that are currently on the
+     * table.
      */
     public void hints() {
         List<Integer> deck = Arrays.stream(slotToCard).filter(Objects::nonNull).collect(Collectors.toList());
         env.util.findSets(deck, Integer.MAX_VALUE).forEach(set -> {
             StringBuilder sb = new StringBuilder().append("Hint: Set found: ");
-            List<Integer> slots = Arrays.stream(set).mapToObj(card -> cardToSlot[card]).sorted().collect(Collectors.toList());
+            List<Integer> slots = Arrays.stream(set).mapToObj(card -> cardToSlot[card]).sorted()
+                    .collect(Collectors.toList());
             int[][] features = env.util.cardsToFeatures(set);
-            System.out.println(sb.append("slots: ").append(slots).append(" features: ").append(Arrays.deepToString(features)));
+            System.out.println(
+                    sb.append("slots: ").append(slots).append(" features: ").append(Arrays.deepToString(features)));
         });
     }
 
@@ -84,51 +99,105 @@ public class Table {
 
     /**
      * Places a card on the table in a grid slot.
+     * 
      * @param card - the card id to place in the slot.
      * @param slot - the slot in which the card should be placed.
      *
      * @post - the card placed is on the table, in the assigned slot.
      */
     public void placeCard(int card, int slot) {
-        try {
-            Thread.sleep(env.config.tableDelayMillis);
-        } catch (InterruptedException ignored) {}
+        synchronized (cardsLock) {
+            try {
+                Thread.sleep(env.config.tableDelayMillis);
+            } catch (InterruptedException ignored) {
+            }
 
-        cardToSlot[card] = slot;
-        slotToCard[slot] = card;
-
+            cardToSlot[card] = slot;
+            slotToCard[slot] = card;
+            env.ui.placeCard(card, slot); // I can only assume this is the way to use the UI
+        }
         // TODO implement
     }
 
     /**
      * Removes a card from a grid slot on the table.
+     * 
      * @param slot - the slot from which to remove the card.
      */
     public void removeCard(int slot) {
         try {
             Thread.sleep(env.config.tableDelayMillis);
-        } catch (InterruptedException ignored) {}
-
+        } catch (InterruptedException ignored) {
+        }
+        synchronized (cardsLock) {
+            int card = slotToCard[slot];
+            slotToCard[slot] = null;
+            cardToSlot[card] = null;
+            env.ui.removeCard(slot);
+        }
         // TODO implement
     }
 
     /**
      * Places a player token on a grid slot.
+     * 
      * @param player - the player the token belongs to.
      * @param slot   - the slot on which to place the token.
      */
     public void placeToken(int player, int slot) {
         // TODO implement
+        synchronized (hashLock) {
+            tokens.put(new Pair(player, slot), true);
+            env.ui.placeToken(player, slot);
+        }
     }
 
     /**
      * Removes a token of a player from a grid slot.
+     * 
      * @param player - the player the token belongs to.
      * @param slot   - the slot from which to remove the token.
-     * @return       - true iff a token was successfully removed.
+     * @return - true iff a token was successfully removed.
      */
     public boolean removeToken(int player, int slot) {
         // TODO implement
+        Pair pair = new Pair(player, slot);
+        synchronized (hashLock) {
+            if (tokens.containsKey(pair)) {
+                env.ui.removeToken(player, slot);
+                tokens.remove(pair);
+                return true;
+            }
+        }
         return false;
+    }
+
+    private class Pair {
+
+        private int first;
+        private int last;
+
+        Pair(int first, int last) {
+            this.first = first;
+            this.last = last;
+        }
+
+        public boolean equals(Object other) {
+            boolean output = false;
+            if (other instanceof Pair) {
+                Pair other2 = (Pair) other;
+                output = other2.getFirst() == this.getFirst() && other2.getLast() == this.getLast();
+            }
+            return output;
+        }
+
+        public int getFirst() {
+            return first;
+        }
+
+        public int getLast() {
+            return last;
+        }
+
     }
 }
